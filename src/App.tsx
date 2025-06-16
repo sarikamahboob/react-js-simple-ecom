25bb7638cbee2c3acee75e4d7f0197a5cfe3ee4f
sarikamahboob <sarika.mahboob1234@gmail.com>
2025-06-17 00:04:05 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import MainContent from './components/MainContent';
+import ProductPage from './components/ProductPage';
+import TopSellers from './components/TopSellers';
+import PopularBlogs from './components/PopularBlogs';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="rounded w-full flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+          <div>
+            <TopSellers />
+            <PopularBlogs />
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App
